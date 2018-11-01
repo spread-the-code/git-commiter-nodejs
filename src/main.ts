9eacba4164401457f2612920a5ac289e75cbc90e
Mosh Feu <moshfeu@gmail.com>
2018-11-01 09:37:14 +0200

    #1 - Add the basic flow

---
+++ src/main.ts
@@ -0,0 +1,52 @@
+import * as bodyParser from 'body-parser';
+import { execSync, spawnSync } from 'child_process';
+import * as express from 'express';
+import { appendFileSync } from 'fs';
+import { join } from 'path';
+import{ directory } from 'tempy';
+
+const app = express();
+const port = 3000;
+const token = '2115bbb1c0fb3fbd9326d097d30430afbcfcf74b';
+
+app.use(bodyParser.json());
+
+app.get('/', (_req, res) => res.send('Hello World!'))
+
+app.post('/commit', (req, res) => {
+  const { remoteRepo } = req.body as {[key: string]: string};
+  const [, repoName] = remoteRepo.split('/');
+
+  const tempFolder = directory();
+
+  const options = (subFolder: string = repoName) => ({
+    encoding: 'utf8',
+    cwd: join(tempFolder, subFolder)
+  });
+
+  const {output} = spawnSync('git', ['clone', `https://${token}@github.com/${remoteRepo}.git`], options(''));
+  console.log(output[2]);
+
+  const n = Date.now();
+  appendFileSync(join(tempFolder, repoName, 'temp'), n);
+
+  console.log('> git add')
+  execSync('git add .', options());
+
+  console.log('> git status')
+  spawnSync('git', ['status'], {...options(),  stdio:'inherit'});
+
+  console.log('> git commit')
+  execSync(`git commit -m "commit now: ${n}"`, {...options(),  stdio:'inherit'});
+
+  console.log('> git push');
+  try {
+    const pushResult = spawnSync('git', ['push'], options());
+    console.log(pushResult.output);
+  } catch (error) {
+    console.log('error', error);
+  }
+  res.send('done');
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
